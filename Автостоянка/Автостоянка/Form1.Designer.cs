046716e29319e8a2bcd5cc1e290e9daeb1d0a428
ovit <ovit@RUBIUS>
2018-02-12 04:35:06 +0600

    *

---
+++ Автостоянка/Автостоянка/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Автостоянка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.номерDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.АвтостоянкаDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.автостоянкаDataSet1 = new Автостоянка.АвтостоянкаDataSet();
+            this.парковочное_местоTableAdapter = new Автостоянка.АвтостоянкаDataSetTableAdapters.Парковочное_местоTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.АвтостоянкаDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.автостоянкаDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.номерDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.АвтостоянкаDataSetBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 449);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowValidated);
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            // 
+            // номерDataGridViewTextBoxColumn
+            // 
+            this.номерDataGridViewTextBoxColumn.DataPropertyName = "Номер";
+            this.номерDataGridViewTextBoxColumn.HeaderText = "Номер";
+            this.номерDataGridViewTextBoxColumn.Name = "номерDataGridViewTextBoxColumn";
+            // 
+            // АвтостоянкаDataSetBindingSource
+            // 
+            this.АвтостоянкаDataSetBindingSource.DataMember = "Парковочное место";
+            this.АвтостоянкаDataSetBindingSource.DataSource = this.автостоянкаDataSet1;
+            // 
+            // автостоянкаDataSet1
+            // 
+            this.автостоянкаDataSet1.DataSetName = "АвтостоянкаDataSet";
+            this.автостоянкаDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // парковочное_местоTableAdapter
+            // 
+            this.парковочное_местоTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 449);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.АвтостоянкаDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.автостоянкаDataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource АвтостоянкаDataSetBindingSource;
+        private АвтостоянкаDataSet АвтостоянкаDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номерDataGridViewTextBoxColumn;
+        private АвтостоянкаDataSet автостоянкаDataSet1;
+        private АвтостоянкаDataSetTableAdapters.Парковочное_местоTableAdapter парковочное_местоTableAdapter;
+    }
+}
+
---
+++ Автостоянка/Автостоянка/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Автостоянка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["ConnectionString"] = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\Автостоянка.accdb";
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "автостоянкаDataSet1.Парковочное_место". При необходимости она может быть перемещена или удалена.
+            this.парковочное_местоTableAdapter.Fill(this.автостоянкаDataSet1.Парковочное_место);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            автостоянкаDataSet1.Парковочное_место.AcceptChanges();
+            автостоянкаDataSet1.AcceptChanges();
+            автостоянкаDataSet1.Парковочное_место.AcceptChanges();
+            //парковочное_местоTableAdapter.Adapter.Fill(автостоянкаDataSet1);
+        }
+
+        private void dataGridView1_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            автостоянкаDataSet1.Парковочное_место.AcceptChanges();
+            автостоянкаDataSet1.AcceptChanges();
+            автостоянкаDataSet1.Парковочное_место.AcceptChanges();
+            //парковочное_местоTableAdapter.Adapter.Fill(автостоянкаDataSet1);
+        }
+    }
+}
